2f566f3cfcdeb3d47969757f9161c5390edabee9
Gerson Quiroa <3005563490101@ingenieria.usac.edu.gt>
2021-06-08 10:41:55 -0600

    menu

---
+++ Practica2/src/Objetos/Almuno.java
@@ -0,0 +1,8 @@
+
+package Objetos;
+
+public class Almuno  {
+    private int carné;
+    private String fechaNac;
+    
+}
--- Practica2/src/main/Menu.java
+++ Practica2/src/main/Menu.java
@@ -1,15 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package main;
 
-/**
- * 
- * @author Sammy Guergachi <sguergachi at gmail.com>
- */
-public class Menu {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class Menu {    
+    public static String[] datos;
+  
+    public static void main(String[] args) {
+        
+        datos = cargaDatos("prueba.csv"); 
+        cargaAlumnos(datos);
+
+    }
+
+    public static String[] cargaDatos(String ruta) {
+        String columnas[] = null;
+        try {         
+            //Leyendo el csv
+            File csv = new File(ruta);
+            Scanner scanner = new Scanner(csv);
+            String datos = "";
+            //Guardando los datos del csv
+            while (scanner.hasNextLine()) {
+                datos += scanner.nextLine() + "\n";
+            }
+            //Partiendo cada dato por medio de comas (,)
+            String filas[] = datos.split("\n");
+            int numFilas = filas.length;
+            int numDatos = numFilas - 1;
+            columnas = filas[0].split(";");
+            int numColumnas = columnas.length;   
+            for (int i = 1; i < numFilas; i++) {
+                columnas = filas[i].split(";");
+            }                  
+        } catch (FileNotFoundException | NumberFormatException e) {
+            System.out.println("No encuentro a tus Pokemones :(");
+            System.out.println("Intenta de nuevo. \n");           
+        }
+         return columnas;
+    }
+    
+    public static void cargaAlumnos(String[] columnas){
+        try{
+            
+            
+        }catch(Exception e){
+            
+        }
+    }
 }
